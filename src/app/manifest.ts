08c9185046cd37567b7414538dce5e1c727ae931
Sukru Kirman <skirman121@gmail.com>
2026-03-24 23:48:54 +0300

    feat: Add PWA icons and update the service worker and manifest.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,19 +11,22 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#141414',
     icons: [
       {
-        src: '/icon.svg',
-        sizes: 'any',
-        type: 'image/svg+xml',
-      },
-      {
-        src: '/icon.svg',
+        src: '/icon-192.png',
         sizes: '192x192',
-        type: 'image/svg+xml',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
-        src: '/icon.svg',
+        src: '/icon-512.png',
         sizes: '512x512',
-        type: 'image/svg+xml',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       }
     ],
   }
